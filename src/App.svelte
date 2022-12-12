981fe790a3016a46d0404f3b3aa0b75e0517f8a2
kazzy-22 <wajahatquazi22@gmail.com>
2022-12-12 15:33:56 +0530

    home

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
      import { Router, Route, Link } from "svelte-navigator";
+     import home from "./home.svelte";
      import login from  "./login.svelte";
      import signup from "./signup.svelte";
+
 </script>
 
 <Router>
     <Route path="/signup" component={signup} />
     <Route path="/login" component={login} />
-
+    <Route path="/home"  component={home}/>
 </Router>
---
+++ src/home.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { navigate } from "svelte-navigator";
+   import "./home.css"
+
+</script>
+
+<div class="container">
+    <div class="top-section">
+        <div class="top-left">
+            <img src="src/images.png" alt="logo">
+        </div>
+        <div class="top-right">
+            <ul id="navbar">
+                <li on:click={() => navigate("/home")} >Home</li>
+                <li  on:click={() => navigate("/login")} >login</li>
+            </ul>
+        </div>
+    </div>
+    <div class="bottom-section">
+        <div class="bottom-text">
+            <h1 id="text1">Think Diffrent</h1>
+            <h4 id="text2">Reduce your carbon footprint in style</h4>
+        </div>
+        <div class="button" on:click={() => navigate("/signup")} >Sign up now</div>
+    </div>
+</div>
